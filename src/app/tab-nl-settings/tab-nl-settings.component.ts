aa8569517f65cb8027a6d542cec2c9442f5e396a
ozokyay <110130153+ozokyay@users.noreply.github.com>
2024-07-05 11:45:35 +0200

    Add some layout settings

--- src/app/tab-nl-settings/tab-nl-settings.component.ts
+++ src/app/tab-nl-settings/tab-nl-settings.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
 
 @Component({
   selector: 'app-tab-nl-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSliderModule,
+    MatSlideToggleModule
+  ],
   templateUrl: './tab-nl-settings.component.html',
   styleUrl: './tab-nl-settings.component.css'
 })
 export class TabNlSettingsComponent {
 
+
+  public onChangeLayout() {
+
+  }
+
+  public onChangeGraphics() {
+
+  }
 }
